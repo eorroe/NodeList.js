2b701dad5740a6e59bb26b840e0dc996ab0d17c1
Edwin Reynoso <eorroe@gmail.com>
2015-07-27 08:08:15 -0400

    Update nodeListLib.js

--- nodeListLib.js
+++ nodeListLib.js
@@ -216,11 +216,11 @@
 	var div = document.createElement('div');
 	for(var prop in div) setterGetter(prop);
 
-	window.$ = function(selector) {
+	window.$$ = function(selector) {
 		var nodes = document.querySelectorAll(selector), newNodes = [];
 		for(var i = 0, l = nodes.length; i < l; i++) newNodes.push(nodes[i]);
 		newNodes.__proto__ = NL;
 		return newNodes;
 	}
-	window.$.NL = NL;
+	window.$$.NL = NL;
 })();
